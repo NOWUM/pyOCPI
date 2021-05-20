5bf7d1cf9e06b7776ad8118cc2579a982b9abed9
Tobias Grüll <gruell@fh-aachen.de>
2021-05-20 15:36:41 +0200

    added model and namespace files for tokens, tariffs and charging_profiles implemented token models

---
+++ ocpi/namespaces/tokens.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 20 11:03:00 2021
+
+@author: gruell
+"""
+
+import logging
+from flask_restx import Resource, Namespace
+from flask_restx import reqparse
+from flask_restx.inputs import datetime_from_iso8601
+from ocpi.models import resp, respList
+from ocpi.decorators import get_header_parser, token_required
+from ocpi.models.location import add_models_to_location_namespace, EVSE, Location, Connector
+from datetime import datetime
+
+
+token_ns = Namespace(name="token", validate=True)
+
+add_models_to_location_namespace(token_ns)
+parser = get_header_parser(token_ns)
+
+log = logging.getLogger('ocpi')
+
+
+def receiver():
+    return token_ns
+
+
+def sender():
+    return token_ns
+
+
+def makeTokenNamespace(interfaces=['SENDER', 'RECEIVER']):
+    log.debug('location interfaces:'+str(interfaces))
+    if 'SENDER' in interfaces:
+        sender()
+    if 'RECEIVER' in interfaces:
+        receiver()
+
+    return token_ns
