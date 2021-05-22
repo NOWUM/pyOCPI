b0235b084ae28df68d5ca6ec3f0da0a75f094d3f
Florian Maurer <maurer@fh-aachen.de>
2021-05-22 15:05:44 +0200

    Add basic TariffElement

--- ocpi/models/tariffs.py
+++ ocpi/models/tariffs.py
@@ -1,1 +1,21 @@
-#TODO: implement tariff models (https://github.com/ocpi/ocpi/blob/master/mod_tariffs.asciidoc)
+# TODO: implement tariff models (https://github.com/ocpi/ocpi/blob/master/mod_tariffs.asciidoc)
+
+from flask_restx import Model, fields
+
+
+tarif_dimension_type = ['ENERGY', 'FLAT', 'PARKING_TIME', 'TIME']
+
+
+PriceComponent = Model('PriceComponent', {
+    'type': fields.String(enum=tarif_dimension_type, reqiored=True, description='Type of tariff dimension.'),
+    'price': fields.Float(required=True, description='Price per unit (excl. VAT) for this tariff dimension.'),
+    'vat': fields.Float(),
+    'step_size': fields.Integer(required=True)
+})
+
+TariffRestrictions = Model('TariffRestrictions', {})
+
+TariffElement = Model('TariffElement', {
+    'price_components': fields.List(fields.Nested(PriceComponent), required=True),
+    'restrictions': fields.Nested(TariffRestrictions),
+})
