ad52912fe67ce3545c5ef9cfdc8a95de62aee75c
Florian Maurer <maurer@fh-aachen.de>
2021-12-01 14:51:21 +0100

    add ConnectorType Model for OCPI Reservation

--- ocpi/models/reservation.py
+++ ocpi/models/reservation.py
@@ -5,7 +5,7 @@ Created on Thu Apr 22 11:05:59 2021
 
 @author: maurer
 """
-from flask_restx import fields
+from flask_restx import fields, Model
 from .sessions import BaseSession, add_models_to_session_namespace
 from .sessions import auth_method, CdrToken, ChargingPeriod, ChargingPreferences
 from .location import connector_type, EnergyMix
@@ -14,20 +14,24 @@ from .tariffs import TariffElement, TariffRestrictions, PriceComponent
 reservation_status = ["REQUEST", "ACTIVE",
                       "COMPLETED", "INVALID", "PENDING", "RESERVATION"]
 
+ConnectorType = Model('ConnectorType', {
+    'connector_type': fields.String(enum=connector_type, max_length=36, required=True, description='Connector.id of an available Connector'),
+    'max_charging_speed': fields.Float(description='Maximum charging speed supported for this connector if lower than technical capabilities'),
+})
+
 Reservation = BaseSession.clone('Reservation', {
     'token_uid': fields.String(required=True, description='The uid of the Token for which this reservation was requested.'),
     'charging_preferences': fields.Nested(ChargingPreferences, required=True, description='Charging Preferences for OCPI'),
     'auth_method': fields.String(enum=auth_method, required=True, description='Method used for authentication.'),
-    'connector_types': fields.List(fields.String(enum=connector_type, max_length=36, required=True, description='Connector.id of the Connector of this Location the charging session is/was happening.')),
+    'connector_types': fields.List(fields.Nested(ConnectorType, description='Supported ConnectorTypes for the reservation')),
     'status': fields.String(enum=reservation_status, default="REQUEST", description='The status of the reservation.'),
     'license_plate': fields.String(description='The optional license plate for recognition at a car park'),
     'price_components': fields.List(fields.Nested(PriceComponent), description="List of Price Components needed to calculate the charging price"),
     'energy_mix': fields.Nested(EnergyMix, description='Name of the energy supplier, delivering the energy for this location or reservation'),
-    'max_charging_speed': fields.Float(description='Maximum charging speed if lower than selected connectors capabilities'),
 })
 
 
 def add_models_to_reservation_namespace(namespace):
-    for model in [CdrToken, ChargingPeriod, Reservation, TariffElement, TariffRestrictions, PriceComponent]:
+    for model in [CdrToken, ChargingPeriod, ConnectorType, Reservation, TariffElement, TariffRestrictions, PriceComponent]:
         namespace.models[model.name] = model
     add_models_to_session_namespace(namespace)
